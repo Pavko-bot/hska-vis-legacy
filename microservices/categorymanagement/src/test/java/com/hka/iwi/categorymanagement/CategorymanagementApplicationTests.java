199a9bd8972e8b03beb6531f93ad5a43edcacd1f
Robin Pavkovic <rpavkovic@inovex.de>
2024-04-02 13:52:15 +0200

    Initialize microservices

---
+++ microservices/categorymanagement/src/test/java/com/hka/iwi/categorymanagement/CategorymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hka.iwi.categorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategorymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ microservices/usermanagement/src/test/java/com/hka/iwi/usermanagement/UsermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hka.iwi.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
